0d0114427a3e84e82c6cdbc53c1abccfdc07c83d
dieg0code <diegoobando20@gmail.com>
2024-09-17 15:15:30 -0300

    trying to resolve ishue with env vars

--- terraform/lambda.tf
+++ terraform/lambda.tf
@@ -8,12 +8,4 @@ resource "aws_lambda_function" "rag_diary" {
   timeout = 160
 
   source_code_hash = filebase64sha256("rag_diary.zip")
-
-  environment {
-    variables = {
-      SUPABASE_URL   = var.supabase_url
-      SUPABASE_KEY   = var.supabase_key
-      OPENAI_API_KEY = var.openai_api_key
-    }
-  }
 }
