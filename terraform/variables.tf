2898c49fc6bc6eab599b01bde6d4112f37ba0301
dieg0code <diegoobando20@gmail.com>
2024-09-17 03:34:45 -0300

    testing

---
+++ terraform/variables.tf
@@ -0,0 +1,14 @@
+variable "supabase_url" {
+  description = "The URL for Supabase"
+  type        = string
+}
+
+variable "supabase_key" {
+  description = "The API key for Supabase"
+  type        = string
+}
+
+variable "openai_api_key" {
+  description = "The API key for OpenAI"
+  type        = string
+}
