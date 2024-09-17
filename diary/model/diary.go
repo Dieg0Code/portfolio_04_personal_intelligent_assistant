5ac44b31dae1e8772806a5f9b4dee5ac0bda10ad
dieg0code <diegoobando20@gmail.com>
2024-09-17 17:12:35 -0300

    fixing ishue with dates

--- diary/model/diary.go
+++ diary/model/diary.go
@@ -10,6 +10,6 @@ type Diary struct {
 	ID        uuid.UUID `json:"id"`
 	Title     string    `json:"title"`
 	Content   string    `json:"content"`
-	CreatedAt time.Time `json:"created_at"`
+	CreatedAt time.Time `json:"created_at" time_format:"2006-01-02 15:04:05.999999999"`
 	Embedding []float32 `json:"embedding"`
 }
