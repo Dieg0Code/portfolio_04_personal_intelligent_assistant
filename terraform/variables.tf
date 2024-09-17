7d451927f790a3cb1df8f11dad6f8ecf76713cda
dieg0code <diegoobando20@gmail.com>
2024-09-17 15:51:57 -0300

    trying to fix ishue with env vars

---
+++ terraform/variables.tf
@@ -0,0 +1,17 @@
+variable "supabase_url" {
+  description = "URL for Supabase"
+  type        = string
+  sensitive   = true
+}
+
+variable "supabase_key" {
+  description = "API key for Supabase"
+  type        = string
+  sensitive   = true
+}
+
+variable "openai_api_key" {
+  description = "API key for OpenAI"
+  type        = string
+  sensitive   = true
+}
