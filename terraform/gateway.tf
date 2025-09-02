b8273ebb71fcec657cb725857db7ac0e91feff04
Dieg0Code <diegoobando20@gmail.com>
2025-09-02 14:24:11 -0400

    change cors

--- terraform/gateway.tf
+++ terraform/gateway.tf
@@ -168,7 +168,7 @@ resource "aws_api_gateway_integration_response" "options_rag_response_integratio
   response_parameters = {
     "method.response.header.Access-Control-Allow-Headers" = "'Content-Type,X-Amz-Date,Authorization,X-Api-Key,X-Amz-Security-Token'"
     "method.response.header.Access-Control-Allow-Methods" = "'GET,OPTIONS,POST,PUT,DELETE'"
-    "method.response.header.Access-Control-Allow-Origin"  = "'https://dieg0code.site'"
+    "method.response.header.Access-Control-Allow-Origin"  = "*"
   }
 }
 
